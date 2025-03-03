a36a5d9a99ec5dcbe8ff5631be04cd39fe8525a9
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2025-03-02 22:24:46 -0500

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,21 +1,85 @@
-def bubble_sort(array)
-  n = array.length
-  loop do
-    swapped = false
-
-    (n - 1).times do |i|
-      if array[i] > array[i + 1]
-        array[i], array[i + 1] = array[i + 1], array[i]  # Swap elements
-        swapped = true
-      end
-    end
-
-    break unless swapped  # If no swaps were made, array is sorted
+class TicTacToe
+  def initialize
+    @board = Array.new(3) { Array.new(3, " ") }
+    @current_player = "X"
   end
 
-  array
+  def play
+    until game_over?
+      display_board
+      player_move
+      switch_player
+    end
+    display_board
+    announce_result
+  end
+
+  private
+
+  def display_board
+    puts "\n  0   1   2"
+    @board.each_with_index do |row, i|
+      puts "#{i} #{row.join(" | ")}"
+      puts "  ---------" unless i == 2
+    end
+    puts
+  end
+
+  def player_move
+    loop do
+      print "#{@current_player}, enter row and column (e.g., 1 2): "
+      input = gets.chomp.split.map(&:to_i)
+
+      if valid_move?(input)
+        @board[input[0]][input[1]] = @current_player
+        break
+      else
+        puts "Invalid move. Try again!"
+      end
+    end
+  end
+
+  def valid_move?(input)
+    return false unless input.length == 2
+    row, col = input
+    return false unless row.between?(0, 2) && col.between?(0, 2)
+    return false unless @board[row][col] == " "
+
+    true
+  end
+
+  def switch_player
+    @current_player = @current_player == "X" ? "O" : "X"
+  end
+
+  def game_over?
+    winner? || draw?
+  end
+
+  def winner?
+    winning_lines.any? { |line| line.uniq.length == 1 && line.first != " " }
+  end
+
+  def draw?
+    @board.flatten.none? { |cell| cell == " " }
+  end
+
+  def winning_lines
+    rows = @board
+    cols = @board.transpose
+    diagonals = [[@board[0][0], @board[1][1], @board[2][2]], [@board[0][2], @board[1][1], @board[2][0]]]
+    rows + cols + diagonals
+  end
+
+  def announce_result
+    if winner?
+      switch_player  # Switch back to the winning player
+      puts "Congratulations, #{@current_player} wins!"
+    else
+      puts "It's a draw!"
+    end
+  end
 end
 
-# Example usage:
-puts bubble_sort([4,3,78,2,0,2]).inspect
-# Output: [0, 2, 2, 3, 4, 78]
+# Start the game
+TicTacToe.new.play
